8efb621e9e6d7b1e34239f68873f49b536782139
s21023 <s21023@std.it-college.ac.jp>
2023-02-28 19:04:50 +0900

    migrationの追加

--- database/migrations/1677578248655_cerfiticate_applications.ts
+++ database/migrations/1677578248655_cerfiticate_applications.ts
@@ -6,12 +6,14 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.integer('certificate_of_enrollment').defaultTo(0)
+      table.integer('transcript').defaultTo(0)
+      table.integer('attendance_certificate').defaultTo(0)
+      table.integer('certificate_of_expected_graduation').defaultTo(0)
+      table.integer('graduation_certificate').defaultTo(0)
+      table.integer('health_certificate').defaultTo(0)
       table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('scheduled_date', {useTz: true})
     })
   }
 
