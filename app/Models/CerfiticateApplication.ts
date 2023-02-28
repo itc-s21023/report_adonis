dae38740a614f3ad2bc8e4e39643cc2d244042bc
s21023 <s21023@std.it-college.ac.jp>
2023-02-28 18:42:12 +0900

    CerfiticateApplication.tsの追加

--- app/Models/CerfiticateApplication.ts
+++ app/Models/CerfiticateApplication.ts
@@ -5,9 +5,27 @@ export default class CerfiticateApplication extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  @column()
+  public certificate_of_enrollment: number
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  @column()
+  public transcript: number
+
+  @column()
+  public attendance_certificate: number
+
+  @column()
+  public certificate_of_expected_graduation: number
+
+  @column()
+  public graduation_certificate: number
+
+  @column()
+  public health_certificate: number
+
+  @column.dateTime({ autoCreate: true })
+  public created_at: DateTime
+
+  @column.dateTime({autoCreate: true})
+  public scheduled_date: DateTime
 }
